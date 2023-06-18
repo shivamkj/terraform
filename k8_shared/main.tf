03685e77647652300f68da092dd2eb694bf61cad
Shivam Kumar Jha <info@shivamjha.com>
2023-06-18 18:26:06 +0530

    kubernetes provider removed

--- k8_shared/main.tf
+++ k8_shared/main.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "hashicorp/helm"
       version = "2.10.1"
     }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.21.1"
-    }
   }
 }
 
